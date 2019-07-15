51506246032493ae7a769973326703a87f4c86db
fatihcelik <fatihcelik@fatihcelik.local>
2019-07-16 01:26:29 +0300

    initial commit

---
+++ src/main/java/com/sexopol/sexopolitics/SexopoliticsApplication.java
@@ -0,0 +1,13 @@
+package com.sexopol.sexopolitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SexopoliticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SexopoliticsApplication.class, args);
+    }
+
+}
